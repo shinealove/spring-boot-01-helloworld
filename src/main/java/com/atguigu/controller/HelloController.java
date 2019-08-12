e65981a50c48957eaf5eb8b987bc3be5f39284fe
shinea_love <shinea_love@sina.cn>
2019-08-12 17:57:50 +0800

    helloController

--- src/main/java/com/atguigu/controller/HelloController.java
+++ src/main/java/com/atguigu/controller/HelloController.java
@@ -3,11 +3,11 @@ package com.atguigu.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
-    @ResponseBody
     @RequestMapping("/hello")
     public String hello(){
         return "Hello World!";
