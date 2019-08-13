3f4467c8a76d27bd07376edd4628137ee5fb0ca3
shinealove <43578700+shinealove@users.noreply.github.com>
2019-08-13 23:10:45 +0800

    logging

--- src/test/java/com/atguigu/HelloWorldApplicationTests.java
+++ src/test/java/com/atguigu/HelloWorldApplicationTests.java
@@ -3,6 +3,8 @@ package com.atguigu;
 import com.atguigu.bean.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -12,6 +14,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class HelloWorldApplicationTests {
 
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     @Autowired
     private Person person;
 
@@ -20,7 +24,13 @@ public class HelloWorldApplicationTests {
 
     @Test
     public void contextLoads(){
-        System.out.println(person);
+//        System.out.println(person);
+        logger.trace("这是 trace 日志。。。。。。。。。。");
+        logger.debug("这是 debug 日志。。。。。。。。。。");
+        logger.info("这是 info 日志。。。。。。。。。。");
+        logger.warn("这是 warn 日志。。。。。。。。。。");
+        logger.error("这是 error 日志。。。。。。。。。。");
+
     }
 
     @Test
